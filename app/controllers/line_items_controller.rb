78326a8c91394116521f4cc7d9713f173ca462d6
Bubbadog <justin.karbowski@gmail.com>
2015-04-20 12:22:57 -0700

    Highlight cart changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-        format.js # rails now will look for create template create.js.erb
+        format.js { @current_item = @line_item }# rails now will look for create template create.js.erb
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
