253f769069e828eb767214f249b5a2bbe910cce1
Bubbadog <justin.karbowski@gmail.com>
2015-04-22 12:35:20 -0700

    Capturing an order

---
+++ app/models/order.rb
@@ -0,0 +1,6 @@
+class Order < ActiveRecord::Base
+  PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase order" ]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+end
