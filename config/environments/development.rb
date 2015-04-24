dbabec6179b8fda654daa5857ed12f20dc569dd6
Bubbadog <justin.karbowski@gmail.com>
2015-04-23 17:38:04 -0700

    Atom feed and setup for confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,23 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Setting up mail server
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               "domain.of.sender.net",
+      authentication:       "plain",
+      user_name:            "daddycanyougetmyname",
+      password:             "Cu8-ddW-Ekk-kbY",
+      enable_starttls_auto: true
+    }
+  end
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
