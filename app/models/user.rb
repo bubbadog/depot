af5c00d56825cfde049dc8ed49820d91eeca3c6e
Bubbadog <justin.karbowski@gmail.com>
2015-05-04 16:10:05 -0700

    sidebar with more admin functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains # after_destroy hook.  Raising an exception rolls back the transaction, and sends it to the controller
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
 end
